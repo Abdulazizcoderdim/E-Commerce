b735a59e650df22c762401cf5dac0cec4d64bb67
AbdulazizRus <coderdim@gmail.com>
2024-05-14 17:00:41 +0500

    chenges

--- lib/models/ProductModel.ts
+++ lib/models/ProductModel.ts
@@ -1,3 +1,21 @@
+import mongoose from "mongoose"
+
+const productSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    slug: {type: String, required: true},
+    category: {type: String, required: true},
+    image: {type: String, required: true},
+    price: {type: Number, required: true},
+    brand: {type: String, required: true},
+    rating: {type: Number, required: true},
+    banner: {type: String, required: true, default:0},
+    numReviews: {type: Number, required: true, default:0},
+    countInStock: {type: Number, required: true, default:0},
+    description: {type: String, required: true},
+    isFeatured: {type: Boolean, required: true},
+})
+
+
 export type Product = {
     _id?: number
     name: string
